17ec1dae2be0b64e85fccab74944a6a139eeaf9d
Visakha Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2017-06-21 16:59:47 -0700

    user sign up page

---
+++ main.py
@@ -0,0 +1,88 @@
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/")
+def index():
+    return render_template('newuser-signup.html')
+
+
+@app.route('/newuser-signup', methods=['POST'])
+def validate_inputs():
+
+    username = request.form['username']
+    password = request.form['password']
+    verify = request.form['verify']
+    email = request.form['email']
+
+    username_error = ''
+    password_error = ''
+    verify_error = ''
+    email_error = ''
+
+#validate username
+    for i in username:
+        if i.isspace():
+            username_error = 'Username must not contain spaces.'
+
+        else:
+            username = str(username)
+            if len(username) < 3 or len(username) > 20:
+                username_error = 'Username must be 3-20 characters'
+
+
+    if not len(username):
+        username_error = 'Not a valid username'
+
+#validate passwords
+    for i in password:
+        if i.isspace():
+            password_error = 'Password must not contain spaces.'
+            password = ''
+        else:
+            password = str(password)
+            if len(password) < 3 or len(password) > 20:
+                password_error = 'Password must be 3-20 characters and not contain spaces.'
+                password = ''
+    if password != verify:
+        verify_error = 'Passwords do not match'
+        verify = ''
+        password = ''
+
+    if not len(password):
+        password_error = 'Not a valid password'
+
+
+#validate email
+    for i in email:
+        if i.isspace():
+            email_error = 'This is not a valid email'
+            if len(email) < 3 or len(email) > 20:
+                email_error = 'Email must be 3-20 characters'
+        else:
+            email = str(email)
+            if "@" not in email:
+                email_error = 'This is not a valid email'
+
+            if "." not in email:
+                email_error = 'This is not a valid email'
+
+#redirect to welcome page
+    if not username_error and not password_error and not verify_error and not email_error:
+        return redirect('/confirmation?username={0}'.format(username))
+    else:
+        return render_template('newuser-signup.html', username_error=username_error,
+            password_error=password_error,
+            verify_error=verify_error, email_error=email_error,
+            username=username, password=password,verify=verify, email=email)
+
+@app.route('/confirmation')
+def welcome():
+    username=request.args.get('username')
+    return render_template('confirmation.html', username=username)
+
+app.run()
